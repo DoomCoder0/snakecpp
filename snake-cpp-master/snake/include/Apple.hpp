a83c9fee41a8765142dd146deb95dbf2016e1dc3
Bruno Obradović <156895426+DoomCoder0@users.noreply.github.com>
2025-02-16 16:51:13 +0100

    Add files via upload

---
+++ snake-cpp-master/snake/include/Apple.hpp
@@ -0,0 +1,20 @@
+#pragma once
+
+#include "Drawable.hpp"
+#include "raylib.h"
+
+namespace snake
+{
+
+class Apple : public Drawable
+{
+  public:
+	explicit Apple(const Vector2 &position = {});
+	Vector2 GetPosition() const;
+	void Draw() const override;
+
+  private:
+	Vector2 _position{};
+};
+
+} // namespace snake
