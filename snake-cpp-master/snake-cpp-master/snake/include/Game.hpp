89bdcc750d397e047def4058d4aae6a2ee337635
Bruno Obradović <156895426+DoomCoder0@users.noreply.github.com>
2025-02-16 16:49:42 +0100

    Add files via upload

---
+++ snake-cpp-master/snake-cpp-master/snake/include/Game.hpp
@@ -0,0 +1,40 @@
+#pragma once
+
+#include "Apple.hpp"
+#include "Drawable.hpp"
+#include "Snake.hpp"
+#include "raylib.h"
+
+namespace snake
+{
+
+class Game : public Drawable
+{
+  public:
+	Game();
+	~Game();
+	void Run();
+
+  private:
+	void Draw() const override;
+	void update();
+	bool shouldMoveSnake() const;
+	void handleSnakeCollision(const Collision collision);
+	void handleSnakeKeyPress();
+	void createSnake();
+	void spawnNewApple();
+	void reset();
+	int currentGameScore() const;
+	void displayScore() const;
+
+  private:
+	Apple _apple;
+	Snake _snake;
+	Direction _nextSnakeDirection = Direction::None;
+	Sound _eatSound;
+	Sound _selfCollisionSound;
+	Sound _wallCollisionSound;
+	bool _isRunning = false;
+};
+
+} // namespace snake
